7b30ca93f3cd009cd59b74a3d1fd5a1a547a9de7
Noel Horváth <noelhorvath69@gmail.com>
2023-06-05 22:23:08 +0200

    add foundation

---
+++ lib/win-api-wrapper/src/win32/foundation.rs
@@ -0,0 +1,34 @@
+use crate::call_BOOL;
+use crate::win32::core::Result;
+use windows_sys::Win32::Foundation::{CloseHandle, GetLastError};
+
+pub use windows_sys::Win32::Foundation::{ERROR_INVALID_HANDLE, ERROR_INVALID_WINDOW_HANDLE};
+
+/// Closes the specified handle.
+///
+/// # Errors
+///
+/// Returns a [`Win32Error`][`crate::win32::core::Win32Error`] if the function fails.
+///
+/// # Possible errors
+///
+/// - `handle` has been already invalidated.
+///
+/// # Examples
+/// TODO
+///
+/// For more information see the official [documentation].
+///
+/// [documentation]: https://learn.microsoft.com/en-us/windows/win32/api/handleapi/nf-handleapi-closehandle
+pub fn close_handle(handle: isize) -> Result<()> {
+    call_BOOL!(CloseHandle(handle))
+}
+
+#[inline]
+#[allow(clippy::undocumented_unsafe_blocks)]
+/// Gets the calling thread's last Win32 error code.
+///
+/// [documentation]: https://learn.microsoft.com/en-us/windows/win32/api/errhandlingapi/nf-errhandlingapi-getlasterror
+pub fn get_last_error() -> u32 {
+    unsafe { GetLastError() }
+}
