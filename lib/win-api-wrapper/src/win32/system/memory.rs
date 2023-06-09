08b4b0f597194f32d6cee81a2c610b0f1d9cb834
Noel Horváth <noelhorvath69@gmail.com>
2023-06-09 03:39:23 +0200

    update memory docs

--- lib/win-api-wrapper/src/win32/system/memory.rs
+++ lib/win-api-wrapper/src/win32/system/memory.rs
@@ -9,6 +9,10 @@ use windows_sys::Win32::System::Memory::{LocalFree, LocalHandle};
 ///
 /// Returns a [`Win32Error`][`crate::win32::core::Win32Error`] if the function fails.
 ///
+/// For more information see the official [documentation].
+///
+/// [documentation]: https://learn.microsoft.com/en-us/windows/win32/api/psapi/nf-psapi-enumprocesses
+///
 pub fn get_local_handle(mem_ptr: *const c_void) -> Result<isize> {
     call_num! { LocalHandle(mem_ptr) != 0 }
 }
@@ -18,6 +22,10 @@ pub fn get_local_handle(mem_ptr: *const c_void) -> Result<isize> {
 /// # Errors
 ///
 /// Returns a [`Win32Error`][`crate::win32::core::Win32Error`] if the function fails.
+///
+/// For more information see the official [documentation].
+///
+/// [documentation]: https://learn.microsoft.com/en-us/windows/win32/api/winbase/nf-winbase-localfree
 ///
 pub fn local_free(local_handle: isize) -> Result<()> {
     call_num! { LocalFree(local_handle) == 0 }
