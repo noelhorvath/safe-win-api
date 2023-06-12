fcdcc76daee117b740dc28adb5dc19df5427b648
Noel Horváth <noelhorvath69@gmail.com>
2023-06-13 01:18:59 +0200

    remove core module from win32

--- lib/win-api-wrapper/src/win32.rs
+++ lib/win-api-wrapper/src/win32.rs
@@ -1,12 +1,8 @@
-/// This module contains the Win32 error and result types;
-pub mod core;
 /// `Win32::Foundation`
 pub mod foundation;
 /// `Win32::Security`
 pub mod security;
 /// `Win32::System`
 pub mod system;
-/*
 /// `Win32::UI`
 pub mod ui;
-*/
