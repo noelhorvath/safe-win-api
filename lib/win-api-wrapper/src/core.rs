c59d888dc20cd61b1460942aab50e194739379ae
Noel Horváth <noelhorvath69@gmail.com>
2023-06-15 21:56:29 +0200

    add get_string_len_from_bytes

--- lib/win-api-wrapper/src/core.rs
+++ lib/win-api-wrapper/src/core.rs
@@ -1,7 +1,8 @@
+pub use windows_sys::core::GUID;
+
 pub mod error;
 
 pub type Result<T> = core::result::Result<T, super::core::error::Error>;
-pub use windows_sys::core::GUID;
 
 #[inline]
 pub fn guid_from_array(bytes: [u8; 16]) -> GUID {
@@ -13,4 +14,32 @@ pub fn guid_from_array(bytes: [u8; 16]) -> GUID {
             bytes[8], bytes[9], bytes[10], bytes[11], bytes[12], bytes[13], bytes[14], bytes[15],
         ],
     }
+}
+
+pub fn get_string_len_from_bytes(buffer: &[u8]) -> usize {
+    let mut len = 0;
+    if buffer.len() >= 2 && buffer[0] != 0 {
+        len += 1;
+    }
+
+    if buffer.len() <= 2 {
+        return len;
+    }
+
+    let buffer_slice = if buffer.len() % 2 == 0 {
+        buffer
+    } else {
+        &buffer[..buffer.len() - 1]
+    };
+    let mut i = 1; // == index of last checked u16 byte's lower byte => [0x11, 0x00] -> 1
+    while i < buffer.len() {
+        i += 1;
+        if buffer_slice[i] == 0 {
+            break;
+        }
+
+        i += 1;
+        len += 1;
+    }
+    len
 }
