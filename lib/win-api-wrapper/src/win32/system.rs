11c76ead439f545d6d98b618d2951446ad8f1b7d
Noel Horváth <noelhorvath69@gmail.com>
2023-06-15 10:27:19 +0200

    export new modules in system

--- lib/win-api-wrapper/src/win32/system.rs
+++ lib/win-api-wrapper/src/win32/system.rs
@@ -1,13 +1,21 @@
+/// `Win32::System::Com`
+pub mod com;
 /// `Win32::System::Diagnostics`
 pub mod diagnostics;
 /// `Win32::System::Kernel`
 pub mod kernel;
+/// `Win32::System::LibraryLoader`
+pub mod library_loader;
 /// `Win32::System::Memory`
 pub mod memory;
+/// `Win32::System::Power`
+pub mod power;
 /// `Win32::System::ProcessStatus`
 pub mod process_status;
 /// `Win32::System::Registry`
 pub mod registry;
+/// `Win32::System::SystemServices`
+pub mod system_services;
 /// `Win32::System::Threading`
 pub mod threading;
 /// `Win32::System::Time`
