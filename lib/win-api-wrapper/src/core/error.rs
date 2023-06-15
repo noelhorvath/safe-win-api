e292d0e20e057763a8b84eeb75334dc2820461e1
Noel Horváth <noelhorvath69@gmail.com>
2023-06-15 10:28:06 +0200

    rewrite Code as enum and improve Error struct

--- lib/win-api-wrapper/src/core/error.rs
+++ lib/win-api-wrapper/src/core/error.rs
@@ -1,18 +1,22 @@
 use crate::common::To;
 use crate::win32::foundation::get_last_error;
 use crate::win32::system::diagnostics::debug::{format_message, FormatMessagetOptions};
+use crate::win32::system::library_loader::{free_library, load_library};
 
-pub const FACILITY_WIN32: u32 = 7;
-
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-pub struct Code(pub(crate) i32);
+#[derive(Copy, Clone, PartialEq, Eq)]
+pub enum Code {
+    Win32(u32),
+    HResult(i32),
+    NtStatus(i32),
+}
 
 impl Code {
-    pub const OK: Self = Code(0);
-
     #[inline]
-    pub const fn is_ok(&self) -> bool {
-        self.0 >= 0
+    pub const fn is_ok(self) -> bool {
+        match self {
+            Self::HResult(val) => val >= 0,
+            _ => self.as_u32() == 0,
+        }
     }
 
     #[inline]
@@ -21,55 +25,108 @@ impl Code {
     }
 
     #[inline]
-    /// Converts a Win32 error code to an [`HRESULT`][windows_sys::core::HRESULT].
-    ///
-    /// For more information see the [`HRESULT_FROM_WIN32`][https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-erref/0c0bcf55-277e-4120-b5dc-f6115fc8dc38] macro specification.
-    ///
-    pub const fn from_win32(code: u32) -> Self {
-        Self(if code == windows_sys::Win32::Foundation::ERROR_SUCCESS {
-            code
+    pub fn from_hresult(value: i32) -> Self {
+        Self::HResult(value)
+    }
+
+    #[inline]
+    pub const fn from_nt(value: i32) -> Self {
+        Self::NtStatus(value)
+    }
+
+    #[inline]
+    pub const fn from_win32(value: u32) -> Self {
+        Self::Win32(value)
+    }
+
+    #[inline]
+    pub const fn as_u32(self) -> u32 {
+        match self {
+            Code::HResult(val) => val as u32,
+            Code::NtStatus(val) => val as u32,
+            Code::Win32(val) => val,
+        }
+    }
+
+    #[inline]
+    pub const fn as_i32(self) -> i32 {
+        match self {
+            Code::HResult(val) => val,
+            Code::NtStatus(val) => val,
+            Code::Win32(val) => val as i32,
+        }
+    }
+
+    #[inline]
+    pub fn is_hresult(self) -> bool {
+        if let Self::HResult(_) = self {
+            true
         } else {
-            (code & 0x0000_FFFF) | (FACILITY_WIN32 << 16) | 0x8000_0000
-        } as i32)
+            false
+        }
     }
 
     #[inline]
-    pub const fn as_u32(&self) -> u32 {
-        self.0 as u32
+    pub fn is_nt(self) -> bool {
+        if let Self::NtStatus(_) = self {
+            true
+        } else {
+            false
+        }
+    }
+
+    #[inline]
+    pub fn is_win32(self) -> bool {
+        if let Self::Win32(_) = self {
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn to_message(self) -> String {
+        if self.is_nt() {
+            // TODO: fix no message
+            let lib_name = widestring::u16cstr!("Ntdll");
+            // Ntdll.dll library is needed to be loaded inorder to get a proper message for an NTSTATUS.
+            // https://stackoverflow.com/questions/25566234/how-to-convert-specific-ntstatus-value-to-the-hresult
+            let module_handle = match load_library(lib_name) {
+                Ok(handle) => handle,
+                Err(err) => {
+                    println!("{:?}", err);
+                    return self.to_string();
+                }
+            };
+            println!("handle: {}", module_handle);
+            let message = format_message(self.as_u32(), 0, None, FormatMessagetOptions::All)
+                .map_or(self.to_string(), |message| message.to_string_lossy());
+            match free_library(module_handle) {
+                _ => message,
+            }
+        } else {
+            format_message(self.as_u32(), 0, None, FormatMessagetOptions::All)
+                .map_or(self.to_string(), |message| message.to_string_lossy())
+        }
     }
 }
 
-impl To<i32> for Code {
-    #[inline]
-    fn to(&self) -> i32 {
-        self.0
-    }
-}
-
-impl To<u32> for Code {
-    #[inline]
-    fn to(&self) -> u32 {
-        self.as_u32()
-    }
-}
-
-impl To<Code> for i32 {
-    #[inline]
-    fn to(&self) -> Code {
-        Code(*self)
-    }
-}
-
-impl To<Code> for u32 {
-    #[inline]
-    fn to(&self) -> Code {
-        Code::from_win32(*self)
+impl core::fmt::Debug for Code {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::HResult(val) => write!(f, "HRESULT({:#010x})", val),
+            Self::NtStatus(val) => write!(f, "NTSTATUS({:#010x})", val),
+            Self::Win32(val) => write!(f, "WIN32_ERROR({:#010x})", val),
+        }
     }
 }
 
 impl core::fmt::Display for Code {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:X}", self.0)
+        match self {
+            Self::HResult(val) => write!(f, "{:#010x}", val),
+            Self::NtStatus(val) => write!(f, "{:#010x}", val),
+            Self::Win32(val) => write!(f, "{:#010x}", val),
+        }
     }
 }
 
@@ -80,13 +137,13 @@ impl To<Error> for Code {
     }
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, PartialEq, Eq)]
 /// Repersents a Windows API error.
 pub struct Error {
-    /// The Windows API error code
-    pub(crate) code: Code,
     /// The Windows API error message
     pub(crate) message: Box<str>,
+    /// The Windows API error code
+    pub(crate) code: Code,
 }
 
 impl Error {
@@ -102,16 +159,13 @@ impl Error {
     /// Creates a new [`Error`] from the specified error code.
     #[inline]
     pub fn from_code(code: Code) -> Self {
-        let message = format_message(code.to(), 0, None, FormatMessagetOptions::All)
-            .map_or(code.to_string(), |message| message.to_string_lossy())
-            .into_boxed_str();
-        Self::new(code, message)
+        Self::new(code, code.to_message().into_boxed_str())
     }
 
     #[inline]
     /// Gets the error code.
     pub const fn code(&self) -> i32 {
-        self.code.0
+        self.code.as_i32()
     }
 
     #[inline]
@@ -141,14 +195,27 @@ impl Error {
 
 impl core::fmt::Display for Error {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        write!(f, "{} ({})", self.message(), self.code)
+        match self.code {
+            Code::HResult(val) => write!(f, "HRESULT: {} ({:#010x})", self.message, val),
+            Code::NtStatus(val) => write!(f, "NTSTATUS: {} ({:#010x})", self.message, val),
+            Code::Win32(val) => write!(f, "WIN32_ERROR: {} ({:#010x})", self.message, val),
+        }
+    }
+}
+
+impl core::fmt::Debug for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Error")
+            .field("code", &self.code)
+            .field("message", &self.message)
+            .finish()
     }
 }
 
 #[doc(hidden)]
 #[macro_export]
 macro_rules! last_error {
-    () => {
+    (Win32) => {
         $crate::core::error::Error::last_win32()
     };
 }
@@ -156,9 +223,13 @@ macro_rules! last_error {
 #[doc(hidden)]
 #[macro_export]
 macro_rules! error_from {
-    ($code:ident) => {
-        $crate::core::error::Error::from_code($crate::common::To::<$crate::core::error::Code>::to(
-            &$code,
-        ))
+    (HResult: $code:expr) => {
+        $crate::core::error::Error::from_code($crate::core::error::Code::from_hresult($code))
+    };
+    (Win32: $code:expr) => {
+        $crate::core::error::Error::from_code($crate::core::error::Code::from_win32($code))
+    };
+    (NtStatus: $code:expr) => {
+        $crate::core::error::Error::from_code($crate::core::error::Code::from_nt($code))
     };
 }
