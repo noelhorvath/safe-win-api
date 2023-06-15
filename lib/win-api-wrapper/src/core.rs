1bd02499ee26733d4280c9d960d3e1b2b4c6dc66
Noel Horváth <noelhorvath69@gmail.com>
2023-06-15 10:24:53 +0200

    add guid_from_array to core

--- lib/win-api-wrapper/src/core.rs
+++ lib/win-api-wrapper/src/core.rs
@@ -1,3 +1,16 @@
 pub mod error;
 
 pub type Result<T> = core::result::Result<T, super::core::error::Error>;
+pub use windows_sys::core::GUID;
+
+#[inline]
+pub fn guid_from_array(bytes: [u8; 16]) -> GUID {
+    GUID {
+        data1: u32::from_le_bytes([bytes[0], bytes[1], bytes[2], bytes[3]]),
+        data2: u16::from_le_bytes([bytes[4], bytes[5]]),
+        data3: u16::from_le_bytes([bytes[6], bytes[7]]),
+        data4: [
+            bytes[8], bytes[9], bytes[10], bytes[11], bytes[12], bytes[13], bytes[14], bytes[15],
+        ],
+    }
+}
